ce1f601b2d22c4ac5ded94f1481e031bc4d2c4d1
kmvignesh <kmvignesh94@gmail.com>
2018-07-15 17:45:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vicky/viewpagerexample/FragmentThree.kt
@@ -0,0 +1,23 @@
+package com.example.vicky.viewpagerexample
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_fragment_one.*
+
+class FragmentThree : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tv_fragment_name.text = "Fragment Three"
+    }
+
+
+}
---
+++ app/src/main/java/com/example/vicky/viewpagerexample/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.vicky.viewpagerexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = MyViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(FragmentOne() , " One ")
+        adapter.addFragment(FragmentTwo() , " Two ")
+        adapter.addFragment(FragmentThree() , " Three ")
+        adapter.addFragment(FragmentFour() , " Four ")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+    }
+
+
+    class MyViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager){
+
+        private val fragmentList : MutableList<Fragment> = ArrayList()
+        private val titleList : MutableList<String> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment,title:String){
+            fragmentList.add(fragment)
+            titleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
+        }
+
+    }
+
+}
